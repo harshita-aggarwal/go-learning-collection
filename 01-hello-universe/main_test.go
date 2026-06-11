0668d35d45f26513a5e10146287f886d1630c115
harshita-aggarwal <haggarw5@asu.edu>
2026-06-11 13:33:16 -0700

    perf: cut the clutter and make tests better

--- 01-hello-universe/main_test.go
+++ 01-hello-universe/main_test.go
@@ -2,78 +2,25 @@ package main
 
 import "testing"
 
-func TestGreet_English (t *testing.T) {
-
-	l := "en"
-
-	got := Greet(l)
-	want := "Hello, Universe!"
-
-	if got != want {
-		t.Errorf("Greet(%q) printed %q; want %q", l, got, want)
+func TestGreet(t *testing.T) {
+	tests := []struct {
+		lang string
+		want string
+	}{
+		{"en", "Hello, Universe!"},
+		{"fr", "Bonjour, Univers!"},
+		{"de", "Hallo, Universum!"},
+		{"id", "Halo, Semesta!"},
+		{"it", "Ciao, Universo!"},
+		{"sv", "Hej, Universum!"},
 	}
-}
-func TestGreet_Indonesian (t *testing.T) {
 
-	l := "id"
+	for _, test := range tests {
+		got := Greet(test.lang)
 
-	got := Greet(l)
-	want := "Halo, Semesta!"
-
-	if got != want {
-		t.Errorf("Greet(%q) printed %q; want %q", l, got, want)
-	}
-}
-func TestGreet_French (t *testing.T) {
-
-	l := "fr"
-
-	got := Greet(l)
-	want := "Bonjour, Univers!"
-
-	if got != want {
-		t.Errorf("Greet(%q) printed %q; want %q", l, got, want)
-	}
-}
-func TestGreet_German (t *testing.T) {
-
-	l := "de"
-
-	got := Greet(l)
-	want := "Hallo, Universum!"
-
-	if got != want {
-		t.Errorf("Greet(%q) printed %q; want %q", l, got, want)
-	}
-}
-func TestGreet_Italian (t *testing.T) {
-
-	l := "it"
-
-	got := Greet(l)
-	want := "Ciao, Universo!"
-
-	if got != want {
-		t.Errorf("Greet(%q) printed %q; want %q", l, got, want)
-	}
-}
-func TestGreet_Swedish (t *testing.T) {
-
-	l := "sv"
-
-	got := Greet(l)
-	want := "Hej, Universum!"
-
-	if got != want {
-		t.Errorf("Greet(%q) printed %q; want %q", l, got, want)
-	}
-}
-func TestGreetUnknownLanguage(t *testing.T) {
-	l := "es"
-	got := Greet(l)
-	want := "I don't know the language code: es"
-
-	if got != want {
-		t.Errorf("Greet(%q) = %q; want %q", l, got, want)
+		if got != test.want {
+			t.Errorf("Greet(%q) = %q; want %q",
+				test.lang, got, test.want)
+		}
 	}
 }
